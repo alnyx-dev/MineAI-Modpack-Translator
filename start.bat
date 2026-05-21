2c0666ffb31b511bcfa40ecfacf4ecfec7ac7369
Thedrezik <44737144+Thedrezik@users.noreply.github.com>
2026-05-22 02:09:51 +0300

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo Установка необходимых библиотек...
-python -m pip install -r requirements.txt
-echo =========================================
-echo Запуск MineAI Translator...
-python translator.py
-pause
